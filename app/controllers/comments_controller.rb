5eb8eb4de2007dcf2dd765cde73d49e1f10d72a9
Nenjamin2405 <nenjamin2405@gmail.com>
2017-04-24 15:40:15 +0700

    finish almost

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
 	def create
     @comment = Comment.new(comment_params)
+    @topic_of_comment = @comment.topic
+    if (!@topic_of_comment.is_final_exam) || (@topic_of_comment.is_final_exam and ((@topic_of_comment.created_at + @topic_of_comment.duration*60) > Time.zone.now))
+      
     if @comment.save
       redirect_to @comment.topic.course
     else
 
       redirect_to @comment.topic.course, :flash => { :warning => 'Something went wrong! Cannot post your comment' }
     end
+ 
+    elsif @topic_of_comment.is_final_exam and ((@topic_of_comment.created_at + @topic_of_comment.duration*60) < Time.zone.now)
+    redirect_to @comment.topic.course, :flash => { :warning => 'Duration of Final Exam is expired!' }
+
     end
+  end
 
   private
 
